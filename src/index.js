a9b0891af2488e0967e87b4964c40c701bd4eb2b
Angelina <tsokur.lina@mail.ru>
2023-08-01 23:31:13 +0300

    feat: function implemented reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let a = 0;
+  let end = 0;
+    if(Math.sign(n) == -1){
+        a = n * (-1);
+  } else a = n;
+  let c = a.toString();
+  let arr = c.split('');
+  for(let i=c.length-1 ;i>=0;--i ){
+    end = end + arr[i];
+  }
+  return end;
 }
